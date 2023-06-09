80e5e9604ade64efa58a6a2452cf99c9511239ec
AOJUCBJD <yumjunstar@naver.com>
2023-06-10 03:14:42 +0900

    SLAM PART ADD

---
+++ src/static_tf/launch/static_tf_launch1.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription(
+        [
+            Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                output="screen",
+                arguments=["0", "0", "0", "0.0", "0.0", "0.0", "map", "odom"],
+            ),
+            Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                output="screen",
+                arguments=[
+                    "0",
+                    "0",
+                    "0",
+                    "0.0",
+                    "0.0",
+                    "0.0",
+                    "base_footprint",
+                    "base_link",
+                ],
+            ),
+            Node(
+                package="tf2_ros",
+                executable="static_transform_publisher",
+                output="screen",
+                arguments=["0", "0", "1", "0.0", "0.0", "0.0", "base_link", "laser"],
+            ),
+        ]
+    )
