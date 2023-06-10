280a3fcb1f6ce05f87b5947192ad300bda71cb95
AOJUCBJD <yumjunstar@naver.com>
2023-06-10 18:40:06 +0900

    Add motor node

--- src/camera/camera/camera.py
+++ src/camera/camera/camera.py
@@ -1,10 +1,15 @@
-import rclpy
-from rclpy.node import Node
-import roboidai as ai
-from sensor_msgs.msg import *
-from std_msgs.msg import *
+import numpy as np
 import cv2
 from cv_bridge import CvBridge
+
+# ROS Library
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import *
+from std_msgs.msg import *
+
+# Beagle Library
+import roboidai as ai
 class camera_pub(Node):
     def __init__ (self, camera_instance_):
         self.camera_instance = camera_instance_
@@ -13,9 +18,8 @@ class camera_pub(Node):
         topic_msg_type = Image # Topic msg type
         topic_msg_name = 'camera_image' # Topic msg name
         qos_profile = 10
-        fps = 1 # 얼마의 주기로 topic msg 를 보낼 것인지 (카메리의 FPS와 동일하게 설정)
-        timer_period = 0.1
-        #fps를 너무 빠르게 하면 못 하는 것 같음
+        fps = 15 # 얼마의 주기로 topic msg 를 보낼 것인지 (카메리의 FPS와 동일하게 설정)
+        timer_period = 1/fps
 
         super().__init__(node_name)
 
@@ -56,9 +60,34 @@ class camera_pub(Node):
         # print (cam_height, cam_width, cam_channel)
         # camera_msg.data = sum(cam_image, []) # 풀어 해진 8bit data #8bit 데이터라서 딱히 변환할 필요 없을 것 같음
 
+def camera_undistort():
 
+
+    dist = np.array([k1, k2, p1, p2, k3])
+    mtx = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]])
+
+    image = cv2.imread('./cali_images/WIN_20230110_13_21_30_Pro.jpg')
+
+    h, w = image.shape[:2]
+
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,
+                                                    dist,
+                                                    (w, h),
+                                                    1, (w, h))
+
+    # undistort
+    dst = cv2.undistort(image, mtx, dist, None, newcameramtx)
+
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+
+    cv2.imshow('result', dst)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return dst
 def main(args = None):
-    camera_type = 'usb0'
+    camera_type = 'ip0'
     cam = ai.Camera(camera_type)
     rclpy.init(args = args)
     node = camera_pub(cam)
---
+++ src/motor/motor/motor.py
@@ -0,0 +1,8 @@
+from roboid import *
+
+
+
+def main():
+    pass
+if __name__ == '__main__':
+    main()
